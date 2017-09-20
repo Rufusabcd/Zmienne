afa47503b397806ac485bdb67a88d3cffdb1bbe8
Marcin Grzeniewski <MarcinGrzeniewski@MacBook-Air-Marcin.local>
2017-09-20 20:44:40 +0200

    zmienne2

---
+++ JavaScript.js
@@ -0,0 +1,8 @@
+var a = 5
+var b = 8
+var value = (a * a) + (2 * a * b) - (b * b) 
+console.log(value)
+var zero = value >= 0 ? 'większe od zera' : 'mniejsze od zera';
+console.log(zero);
+var count = value == 0 ? 'równe zero' : 'nierówne zero'
+console.log(count)
